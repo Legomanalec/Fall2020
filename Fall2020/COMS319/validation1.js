901f859f8a3a57fba13a9797760c72b9b64b41fe
Legomanalec <48848098+Legomanalec@users.noreply.github.com>
2020-09-07 00:21:45 -0500

    9/7/2020

--- Fall2020/COMS319/validation1.js
+++ Fall2020/COMS319/validation1.js
@@ -1,1 +1,50 @@
-var x = 1;
+function validateForm(correct, wrong, nextWindow)
+{
+	var alertStatus = false;
+	var alertStr = "";
+	
+	fieldCheck("first", correct, wrong);
+	fieldCheck("last", correct, wrong);
+	fieldCheck("gender", correct, wrong);
+	fieldCheck("state", correct, wrong);
+	
+	if(alertStatus)
+	{
+		alert(alertStr);
+		return false;
+	}
+	else
+	{
+		window.open(nextWindow);
+	}
+}
+
+//This function checks the status of each field.
+function fieldCheck(fieldId, correct, wrong)
+{
+	var temp = document.forms["form"][fieldId].value;
+	
+	if (temp == "")
+	{
+		alertStatus = true;
+		alertStr = fieldId + " cannot be blank \n";
+		imagePlace(wrong, fieldId);
+	}
+	else
+	{
+		imagePlace(correct, fieldId);
+	}
+}
+
+//This function adds/removes the icons next to each field
+function imagePlace(source, option)
+{
+	var img = document.createElement("img");
+	var divElement = document.getElementById(option);
+	img.src = source;
+	
+	//removes the images so they don't stack
+	divElement.removeChild(divElement.lastChild);
+	
+	divElement.appendChild(img);
+}
