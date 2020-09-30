2a41621cf47690ee40ecb10957084e9d7737d95f
Legomanalec <48848098+Legomanalec@users.noreply.github.com>
2020-09-30 15:32:18 -0500

    Finished question a, b, c

--- Fall2020/COMS363/Homework2/alecrm4Q1.sql
+++ Fall2020/COMS363/Homework2/alecrm4Q1.sql
@@ -6,9 +6,22 @@ COUNT(*) AS numOfIngredient
 FROM ingredient;
 
 -- part b.
-SELECT food.fid, ingredient.iid
-FROM food, ingredient
-LEFT JOIN recipe ON food.fid = recipe.fid and ingredient.iid = recipe.iid
-WHERE ingredient.iname = "Green Onion" and ingredient.iname = "Beef"
+SELECT f.fid, f.fname
+FROM food f
+INNER JOIN recipe r ON f.fid = r.fid
+INNER JOIN ingredient i ON r.iid = i.iid
+WHERE i.iname = 'Green Onion' AND f.fid IN
+(SELECT f.fid
+	FROM food 
+    INNER JOIN recipe r ON f.fid = r.fid
+    INNER JOIN ingredient i2 ON r.iid = i2.iid
+    WHERE i2.iname = 'Beef');
 
 -- part c.
+SELECT i.iid, i.iname
+FROM ingredient i
+LEFT JOIN recipe r ON r.iid = i.iid
+WHERE r.iid is NULL
+
+-- part d. 
+SELECT f.fid, f.fname,
