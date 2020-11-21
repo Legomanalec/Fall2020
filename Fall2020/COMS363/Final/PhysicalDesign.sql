e37bfbfcb7053244fe966dfa15c05760fbe4c26f
Alec Meyer <alecrmeyer4@gmail.com>
2020-11-21 13:43:41 -0600

    hopefully this works

--- Fall2020/COMS363/Final/PhysicalDesign.sql
+++ Fall2020/COMS363/Final/PhysicalDesign.sql
@@ -1,28 +1,33 @@
--- Q9 Index
+	-- Q9 Index
 CREATE INDEX subCategoryInd
 ON users (subcategory);
+
 ALTER TABLE users
 DROP INDEX subCategoryInd;
 
 -- Q7 Index
 CREATE INDEX stateInd
 ON users (state);
+
 ALTER TABLE users
 DROP INDEX stateInd;
 
 -- Q18 Index
 CREATE INDEX screenNameInd
 ON users (screenName);
+
 ALTER TABLE users
 DROP INDEX screenNameInd;
 
 -- Q16 Index
 CREATE INDEX createdTimeInd
 ON tweets (createdTime);
+
 ALTER TABLE tweets
 DROP INDEX createdTimeInd;
 
 -- Q3 and Q23 Buffer pool change
 SHOW VARIABLES LIKE 'innodb_buffer_pool_size';
 SET GLOBAL innodb_buffer_pool_size = 1073741824;
+
 SET GLOBAL innodb_buffer_pool_size = 8388608;
